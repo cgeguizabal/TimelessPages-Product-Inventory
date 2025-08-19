c1f8f8e27559daf0057ec931f1236c4be689e5c1
Guillermo Eguizabal <cgeguizabal@gmail.com>
2025-08-19 17:28:36 -0600

    Implement user authentication: register, login, logout

---
+++ TimelessPages-Backend/app/Http/Controllers/API/AuthController.php
@@ -0,0 +1,98 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash; // I added it so I could hash Password
+use Illuminate\Support\Facades\Validator;
+
+
+class AuthController extends Controller
+{
+    //Register a new user
+    public function register(Request $request){
+
+    // Validate the input from request       
+        $validator = Validator::make($request->all(),[
+            'name' => 'required|string|max:255',
+            'email' => 'required|email|unique:users,email',
+            'password' => 'required|string|min:6|confirmed', // add password_confirmation on request
+            'role' => 'in:admin,seller', //Can be either admin or seller, nothing else
+        ]);
+    
+    //Handle errors
+        if($validator->fails()){
+            $errorMessage = $validator->errors()->first();
+            $response = [
+                "status" => false,
+                "message" => $errorMessage,
+            ];
+            return response()->json($response, 422);
+        }
+
+     //Create User and hash password
+        $user = User::create([
+        'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password),
+            'role' => $request->role ?? 'seller', 
+    ]);
+
+    //Create token 
+        $token = $user->createToken('auth_token')->plainTextToken;
+
+    //Response
+    return response()->json([
+        "message" => "User registered successfully",
+        'user' => $user,
+            'token' => $token,
+            'token_type' => 'Bearer' 
+    ],201);
+}
+
+public function login(Request $request){
+
+    //Validate input request
+    $validator = Validator::make($request->all(), [
+        'email' => "required|string|email",
+        "password" => "required",
+    ]);
+    
+    //Handle errors
+    if($validator->fails()){
+        $errorMessage = $validator->errors()->first();
+        $response = [
+            'status' => false,
+            'message'=> $errorMessage,
+        ];
+        return response()->json($response, 401);
+    }
+
+    //Find user by email
+    $user = User::where('email', $request->email)->first();
+
+    //Check password
+    if(!$user || !Hash::check($request->password, $user->password)){
+    return response()->json([
+        'status' => false,
+        "message" => 'The provided credentials are incorrect'
+    ], 401);
+}
+
+    //Delete old tokens
+    $user->tokens()->delete();
+
+    //Create new token
+    $token = $user->createToken('auth_token')->plainTextToken;
+
+    //Send response(token)
+    return response()->json([
+        'user' => $user,
+        'token' => $token,
+        'token_type' => 'Bearer'
+    ]);    
+}
+
+}

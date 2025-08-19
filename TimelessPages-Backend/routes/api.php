e5b067cdcd16add9ba8eea3426df77ce06c16dbd
Guillermo Eguizabal <cgeguizabal@gmail.com>
2025-08-19 17:56:24 -0600

    Add and protect Log out EndPoint

--- TimelessPages-Backend/routes/api.php
+++ TimelessPages-Backend/routes/api.php
@@ -12,3 +12,4 @@ use App\Http\Controllers\API\AuthController;
 //Authentication(Register, Login and Logout)
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
+Route::middleware('auth:sanctum')->post('/logout', [AuthController::class, 'logout']);

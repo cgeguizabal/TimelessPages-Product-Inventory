df4ec5f0715d344174bcfdb893272b45891113ed
Guillermo Eguizabal <cgeguizabal@gmail.com>
2025-08-21 17:35:26 -0600

    Added API services for authentication

---
+++ TimelessPages-Frontend/src/api/services/auth.js
@@ -0,0 +1,40 @@
+import axios from "axios";
+
+const BASE_URL =
+  "https://timelesspages-product-inventory-production.up.railway.app/api";
+
+export async function registerUser(data) {
+  try {
+    const response = await axios.post(`${BASE_URL}/register`, data, {
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+
+    return response.data;
+  } catch (error) {
+    console.error(
+      "Error registering user:",
+      error.response?.data || error.message
+    );
+    throw error.response?.data || error;
+  }
+}
+
+export async function login(data) {
+  try {
+    const response = await axios.post(`${BASE_URL}/login`, data, {
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+
+    return response.data;
+  } catch (error) {
+    console.error(
+      "Error registering user:",
+      error.response?.data || error.message
+    );
+    throw error.response?.data || error;
+  }
+}

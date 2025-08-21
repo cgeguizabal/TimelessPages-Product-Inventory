8db5ccc54c5982a39bae5e0fbd52e4e952c60ab5
Guillermo Eguizabal <cgeguizabal@gmail.com>
2025-08-21 17:35:43 -0600

    Added zustand store with auth methods

---
+++ TimelessPages-Frontend/src/store/auth.js
@@ -0,0 +1,22 @@
+import { create } from "zustand";
+import { persist } from "zustand/middleware";
+
+const useAuthStore = create(
+  persist(
+    (set) => ({
+      user: null,
+      token: null,
+      isAuthenticated: false,
+
+      // actions
+      login: (user, token) => set({ user, token, isAuthenticated: true }),
+
+      logout: () => set({ user: null, token: null, isAuthenticated: false }),
+    }),
+    {
+      name: "auth-storage", // key in localStorage
+    }
+  )
+);
+
+export default useAuthStore;

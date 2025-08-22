bccd43292f8f8ed3e558236c9235d5f3a480485c
Guillermo Eguizabal <cgeguizabal@gmail.com>
2025-08-21 20:11:20 -0600

    Added boxbutton components

---
+++ TimelessPages-Frontend/src/components/BoxButton.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import boxbutton from "../styles/components/boxButton.module.scss";
+
+function BoxButton({ title, className }) {
+  return (
+    <div className={` ${className} ${boxbutton.box_button}`}>
+      <h1>{title}</h1>
+    </div>
+  );
+}
+
+export default BoxButton;

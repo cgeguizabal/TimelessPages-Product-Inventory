396cab54dedb12d15551803899b41ed5414e9587
Guillermo Eguizabal <cgeguizabal@gmail.com>
2025-08-26 14:39:11 -0600

    Improved with axiosInstace

--- TimelessPages-Frontend/src/api/services/salesService.js
+++ TimelessPages-Frontend/src/api/services/salesService.js
@@ -1,9 +1,14 @@
 import axios from "axios";
+import { apiPost } from "./axiosInstance";
 
 const BASE_URL =
   "https://timelesspages-product-inventory-production.up.railway.app/api";
 
-export const createSale = async (token, data) => {
+export const createSale = (data) => {
+  //Add token and async when using normal axios without axiosInstance
+
+  return apiPost("/sales", data);
+  /* //Use in the case there's individual logic from axiosIntances
   try {
     const response = await axios.post(`${BASE_URL}/sales`, data, {
       headers: {
@@ -19,10 +24,13 @@ export const createSale = async (token, data) => {
       error.response?.data || error.message
     );
     throw error.response?.data || error.message;
-  }
+  }*/
 };
 
-export async function fetchSalesReport(token, data) {
+export async function fetchSalesReport(data) {
+  //Add token and async when using normal axios without axiosInstance
+  return apiPost("/reports/sales", data);
+  /* //Use in the case there's individual logic from axiosIntances
   try {
     const response = await axios.post(`${BASE_URL}/reports/sales`, data, {
       headers: {
@@ -37,5 +45,5 @@ export async function fetchSalesReport(token, data) {
       error.response?.data || error.message
     );
     throw error.response?.data || error.message;
-  }
+  }*/
 }

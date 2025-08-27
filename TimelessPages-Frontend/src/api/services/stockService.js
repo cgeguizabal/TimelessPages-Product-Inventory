f571764be833f9deda1727b6bc8ff3a2b97f176f
Guillermo Eguizabal <cgeguizabal@gmail.com>
2025-08-26 23:28:34 -0600

    added v1

--- TimelessPages-Frontend/src/api/services/stockService.js
+++ TimelessPages-Frontend/src/api/services/stockService.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const BASE_URL =
-  "https://timelesspages-product-inventory-production.up.railway.app/api";
+  "https://timelesspages-product-inventory-production.up.railway.app/api/v1";
 
 export const getStockReport = async (token) => {
   try {

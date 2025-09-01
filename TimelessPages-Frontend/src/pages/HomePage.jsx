6d6b4e95fae6d0ab6074df97ed958ea8f4d65596
Guillermo Eguizabal <cgeguizabal@gmail.com>
2025-09-01 17:11:01 -0600

    Updated with Navbar component

--- TimelessPages-Frontend/src/pages/HomePage.jsx
+++ TimelessPages-Frontend/src/pages/HomePage.jsx
@@ -3,8 +3,8 @@ import { useNavigate } from "react-router-dom";
 import useAuthStore from "../store/auth";
 import home from "../styles/page/home.module.scss";
 import BoxButton from "../components/BoxButton";
-import { logout } from "../api/services/auth";
-import { motion } from "motion/react";
+
+import Navbar from "../components/Navbar";
 
 function HomePage() {
   const { user, token, isAuthenticated, logout: clearAuth } = useAuthStore();
@@ -19,60 +19,9 @@ function HomePage() {
 
   if (!isAuthenticated) return null;
 
-  const handleLogout = async () => {
-    try {
-      await logout(); // try to log out on backend //Add token when using normal axios without axiosInstance
-    } catch (error) {
-      // If token is invalid/expired → just clear locally
-      if (error.response?.status === 401) {
-        console.warn("Token expired or invalid. Clearing locally.");
-      } else {
-        console.error("Logout failed:", error);
-        alert("Something went wrong. Logging you out locally.");
-      }
-    } finally {
-      // Always clear client-side no matter what
-      clearAuth();
-      navigate("/login");
-    }
-  };
-
   return (
     <>
-      <div className={home.home_container}>
-        <h1 className={home.hidden}>Welcome {user?.name}</h1>
-        <div className={home.home_buttons}>
-          {user.role === "admin" && (
-            <motion.div
-              initial={{ opacity: 0, x: 100 }}
-              animate={{ opacity: 1, x: 0 }}
-              transition={{
-                delay: 0.1,
-                x: { duration: 0.5, ease: "easeInOut" },
-                opacity: { duration: 1.5, ease: "easeOut" },
-              }}
-              onClick={() => navigate("/dashboard")}
-              className={home.dashboard}
-            >
-              <h2>Dashboard</h2>
-            </motion.div>
-          )}
-          <motion.button
-            initial={{ opacity: 0, x: 105 }}
-            animate={{ opacity: 1, x: 0 }}
-            transition={{
-              delay: 0.9,
-              x: { duration: 0.5, ease: "easeInOut" },
-              opacity: { duration: 1.5, ease: "easeOut" },
-            }}
-            onClick={handleLogout}
-            className={home.logout}
-          >
-            <h2>Logout</h2>
-          </motion.button>
-        </div>
-      </div>
-
+      <Navbar />
       <div className={home.home_container2}>
         <div className={home.home_image_container}>
           <img src="timelessPages2.png" alt="logo" />
